165a953ec37ee04a597384de38e6536ab015c525
Pekka Nikander <pekka.nikander@iki.fi>
2018-05-22 13:06:53 +0300

    Add a comment that IWebResource should be replaces with an IHttpHandler interface

--- UniWebServer/Assets/UniWebServer/Lib/IWebResource.cs
+++ UniWebServer/Assets/UniWebServer/Lib/IWebResource.cs
@@ -1,6 +1,11 @@
 using UnityEngine;
 using System.Collections;
 
+/*
+ * XXX: Should be replaced with IHttpHandler
+ * See https://msdn.microsoft.com/en-us/library/system.web.ihttphandler.aspx
+ */
+
 namespace UniWebServer
 {
     public interface IWebResource
